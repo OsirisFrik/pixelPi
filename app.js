aebdfa8c06f09a7f73dce1ae2436c3e6b004378b
OsirisFrik <jlopez@jelp.io>
2017-10-10 14:26:52 -0700

    test 2

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ var strip = null;
 
 board.on("ready", function() {
 
-    strip = new pixel.Strip({
-        board: this,
-        controller: "FIRMATA",
-        strips: [ {pin: 7, length: 3}, ], // this is preferred form for definition
-        gamma: 2.8, // set to a gamma that works nicely for WS2812
-    });
+  strip = new pixel.Strip({
+    board: this,
+    controller: "FIRMATA",
+    strips: [
+      {
+        pin: 7,
+        length: 3
+      }
+    ], // this is preferred form for definition
+    gamma: 2.8, // set to a gamma that works nicely for WS2812
+  });
 
-    strip.on("ready", function() {
-        // do stuff with the strip here.
-    });
+  strip.on("ready", function() {
+    strip.pixel(0).color("#000");
+    strip.pixel(1).color("red");
+    strip.shift(1, pixel.FORWARD, true);
+    strip.pixel(1).color; // will now be nothing
+    strip.pixel(2).color; // will now be red.
+  });
 });
